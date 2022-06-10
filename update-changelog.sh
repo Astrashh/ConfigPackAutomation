ca6c1bea96dcf9564065b97f0400b9e80035e657
Astrash <astrash@protonmail.com>
2022-06-10 21:38:43 +1000

    Comment out unreleased headings

--- update-changelog.sh
+++ update-changelog.sh
@@ -17,7 +17,7 @@ sed "1,/$START_DELIMITER/ d; /$END_DELIMITER/,$ d" $changelog > $new_changelog
 # Remove unused subheadings from new changelog
 echo '- Stripping empty titles from changelog'
 echo "$(awk '/^$/ {if (i) {b=b $0 "\n"} else {print $0 }; next} \
-    /^###/ {i=1; b=$0; next} {if (i) {print b}; i=0; print $0; next}' $new_changelog)" > $new_changelog 
+    /^(?:<!-- )?###/ {i=1; b=$0; next} {if (i) {print b}; i=0; print $0; next}' $new_changelog)" > $new_changelog 
 
 if ! grep -q '[^[:space:]]' "$new_changelog"; then
     echo '  - WARNING: Unreleased changelog is empty!'
@@ -28,16 +28,16 @@ echo "- Updating $changelog file:"
 echo '  - Resetting unreleased changelog'
 sed -ni "1,/$START_DELIMITER/ p; /$END_DELIMITER/,$ p" $changelog
 sed -i "/$START_DELIMITER/ {
-        a ### Added
+        a <!-- ### Added -->
         a
         a
-        a ### Changed
+        a <!-- ### Changed -->
         a
         a
-        a ### Removed
+        a <!-- ### Removed -->
         a
         a
-        a ### Fixed
+        a <!-- ### Fixed -->
         a
         a
     }" $changelog
