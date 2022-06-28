68d53cdfaecbdb24f1e9ba21252e99eb166966df
Astrash <astrash@protonmail.com>
2022-06-28 18:33:37 +1000

    Test biome list automation

--- .scripts/generate-biome-list.sh
+++ .scripts/generate-biome-list.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
 mkdir .docs
-find biomes -name "*.yml" -not -path "biomes/abstract/*" > .docs/biome-list.txt
+LIST='biome-list.txt'
+DOC='.docs/biome-list.md'
+
+find biomes/*/ -name "*.yml" -not -path "biomes/abstract/*" | sed "s|^biomes/||" > $LIST
+
+rm $DOC
+touch $DOC
+echo '# Biome List' >> $DOC
+echo '' >> $DOC
+
+LIST_CONTENTS="$(cat $LIST)"
+for path in $LIST_CONTENTS
+do
+    IFS="/" read -a items <<< $path # Split path into a list
+    file_name="${items[-1]}"
+    for (( i=0; i < ${#items[@]}; i++ ))
+    do
+        items[$i]=$(sed 's/.yml//; s/^./\u&/; s/_\(.\)/ \u\1/g' <<< ${items[$i]}) # Humanize each list item
+    done
+    biome_name=${items[-1]}
+    unset items[-1]
+    echo "- $biome_name" >> $DOC
+done
