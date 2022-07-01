7d8199edf760cf73d5a210415ca538cd2672bf87
Astrash <astrash@protonmail.com>
2022-07-01 14:54:19 +1000

    Use wiki deploy script

---
+++ .scripts/upload-to-wiki.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$GITHUB_ACTIONS" != true ]; then
+    echo "This script must be ran via GitHub actions."
+    exit 1
+fi
+
+echo "Deleting .git folder..."
+rm -rf .git
+
+echo "Setting up temporary repository..."
+cd .wiki
+git init
+
+echo "Setting up authentication..."
+git config user.name ${LOGIN_NAME}
+git config user.email ${LOGIN_EMAIL}
+
+echo "Committing changes..."
+git add *
+git commit -m "GitHub Action Deployment"
+
+echo "Pushing to wiki..."
+git remote add origin https://${ACCESS_TOKEN}@github.com/$1
+git push origin master --force
+
