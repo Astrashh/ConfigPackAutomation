bd0b1754765d8e448270e3b5a0c634f26ac8fc81
Astrash <astrash@protonmail.com>
2022-07-18 14:25:48 +1000

    Color validation script improvements

--- .scripts/validate-biome-colors.sh
+++ .scripts/validate-biome-colors.sh
@@ -2,18 +2,19 @@
 biome_config_paths="$(find biomes/*/ -name "*.yml" -not -path "biomes/abstract/*")"
 errors=''
 fail=false
+color_key='color: \$biomes/colors.yml:'
 for path in $biome_config_paths
 do
     config_id_line="$(grep 'id: ' $path)"
     config_id="${config_id_line#id: }"
     color_line="$(grep 'color: ' $path)"
-    color_id="${color_line#color: \$biomes/colors.yml:}"
-    if ! grep -q '[^[:space:]]' <<< $color_line; then
+    color_id="${color_line#$color_key}"
+    if [[ $color_line != *$color_key* ]]; then
         fail=true
-        errors+="\n$config_id - does not contain a colors key."
+        errors+="\n\n$path\nDoes not contain a valid color key"
     elif [[ $color_id != $config_id ]]; then
         fail=true
-        errors+="\n$config_id color '$color_id' does not match biome id."
+        errors+="\n\n$path\nColor reference '$color_id' does not match biome id '$config_id'"
     fi
 done
 if [[ $fail == true ]]; then
